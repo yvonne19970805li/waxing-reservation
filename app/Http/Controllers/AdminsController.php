9336e69b2e86d3d8075bd0e02bd12829fb7f820d
yvonne19970805li <yujhen0805@gmail.com>
2025-06-12 01:07:49 +0800

    Add: 新增admin帳號密碼驗證

---
+++ app/Http/Controllers/AdminsController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use app\Repositories\AdminsRepository;
+use Illuminate\Auth\Events\Login;
+use Illuminate\Support\Facades\Hash;
+
+
+class AdminsController extends Controller
+{
+    /**
+     * 驗證管理者登入資料
+     */
+    public function AdminLogin(Request $request)
+    {
+        //檢查帳號是否存在  可以怎麼把它縮短嗎？
+        $account = $request->input('account', '');
+        $accountCheck = AdminsRepository::AccountCheck($account);
+        if(is_null($accountCheck)){
+            return '帳號錯誤或不存在';
+        }
+
+        //如果帳號存在則繼續檢查密碼
+        $password = $request->input('password', '');
+        $hashpassword = AdminsRepository::Login($account);
+        if (Hash::check($password, $hashpassword)) {
+            redirect('/');
+        } else {
+            return '密碼錯誤';
+        }
+    }
+}
--- app/Repositories/AdminsRepository.php
+++ app/Repositories/AdminsRepository.php
@@ -2,11 +2,22 @@
 
 namespace App\Repositories;
 
+use Illuminate\Support\Facades\Hash;
+
+
 use App\Models\Admins;
 
 class AdminsRepository
 {
+    //檢查帳號是否存在
+    static function AccountCheck($account){
+        return Admins::where('account' , $account)->first();
+    }
 
+    //  抓出對應帳號的密碼回傳
+    static function Login($account)
+    {
+        Admins::where('account', $account)
+            ->value('password');
+    }
 }
-
-?>
